78e76e450777a6f918ea2030a9ce80ebba3defd6
Mads Engel Lundt <madslundt@live.dk>
2019-01-31 13:30:18 +0100

    Update tree.ts

--- Tree/tree.ts
+++ Tree/tree.ts
@@ -4,13 +4,13 @@ interface ITree<T> {
 }
 
 /**
- * Takes a list of items and returns a nested list in a tree structure where level is included
- * @param {T[]} list - Input list
- * @param {string} keyId - Key for each item's unique id in the list
+ * Takes a list of items and returns a nested list in a tree structure where level is included.
+ * @param {T[]} list - Input list containing a unique key and a parent key.
+ * @param {string} keyId - Key for each item's unique id in the list.
  * @param {string} parentKeyId - Key for each item's parent id in the list. 
  * @param {string | number} parentId - Starting parent id. Null is allowed for root.
- * @param {number} maxLevel - Number of max levels. Levels greater than maxLevel is placed in maxLevel
- * @param {number} level - The starting level. Default 0
+ * @param {number} maxLevel - Number of max levels. Levels greater than maxLevel is not nester further.
+ * @param {number} level - The starting level. Default 0.
  */
 const getListToTree = <T>(list: T[], keyId: string, parentKeyId: string, parentId: string | number, maxLevel: number, level: number = 0): ITree<T>[] => {
     let result: ITree<T>[] = [];
