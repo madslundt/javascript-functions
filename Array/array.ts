a8f7391f15bca8336b4bb093f0b91282112ff988
Mads Engel Lundt <madslundt@live.dk>
2019-03-02 21:23:29 +0100

    Create array.ts

---
+++ Array/array.ts
@@ -0,0 +1,41 @@
+/**
+ * Takes a list of items and returns a list of unique items.
+ * @param {any[]} list - Input list.
+ */
+const removeDuplicates = (arr: any[]): any[] => arr.filter((item, index) => index === arr.indexOf(item));
+const removeDuplicates2 = (arr: any[]): any[] => [...new Set(arr)];
+
+/**
+ * Takes a list with nested lists and flattens the list at one level.
+ * @param {any[]} list - Input list.
+ */
+const flatten = (arr: any[]) => [].concat(...arr);
+
+/**
+ * Takes a list with nested lists and flattens the list at any level.
+ * @param {any[]} list - Input list.
+ */
+const flatten2 = (arr: any[]) => arr.reduce((prevVal, curVal) => prevVal.concat(Array.isArray(curVal) ? flatten2(curVal) : curVal));
+
+/**
+ * Merge multiple lists
+ */
+const merge = (arr: any[]): any => [].concat(...arr);
+
+/**
+ * Pipe output of one function to another function.
+ */
+const pipe = (...fns) => arg => fns.reduce((v, fn) => fn(v), arg);
+
+/**
+ * Generate range from start to end
+ * @param {number} start - Start number.
+ * @param {number} end - End number.
+ * @param {number} step - Step level. Default 1. 
+ */
+const range = (start: number, end: number, step = 1) => Array.from({length: Math.ceil((end - start) / step)}, (_, i) => start + i * step);
+
+/**
+ * Generate random color
+ */
+const color = () => `#${Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, '0')}`;
