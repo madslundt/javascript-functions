b0d2b322a6d5a2673149f4ab1432739e2eb494e4
Mads Engel Lundt <madslundt@live.dk>
2019-05-24 10:47:51 +0200

    Create sort.ts

---
+++ Algorithms/sort.ts
@@ -0,0 +1,44 @@
+const merge = <T>(leftSide: T[], rightSide: T[]): T[] => {
+    let result: T[] = [];
+    let leftIndex: number = 0;
+    let rightIndex: number = 0;
+
+    const leftLength = leftSide.length;
+    const rightLength = rightSide.length;
+
+    while (leftIndex < leftLength && rightIndex < rightLength) {
+        const leftItem = leftSide[leftIndex];
+        const rightItem = rightSide[rightIndex];
+
+        if (leftItem < rightItem) {
+            result.push(leftItem);
+            leftIndex++;
+        } else {
+            result.push(rightItem);
+            rightIndex++;
+        }
+    }
+
+    if (leftIndex < leftLength) {
+        return [...result, ...leftSide.slice(leftIndex)];
+    } else {
+        return [ ...result, ...rightSide.slice(rightIndex)]
+    }
+}
+
+const mergeSort = <T>(arr: T[]): T[] => {
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    const middleIndex = Math.floor(arr.length / 2);
+    const leftSide = arr.slice(0, middleIndex);
+    const rightSide = arr.slice(middleIndex);
+
+    return merge(mergeSort(leftSide) || [], mergeSort(rightSide) || []);
+}
+
+
+export {
+    mergeSort,
+}
