6588b172b82c0add4835a1b3c07ace46efc88bdc
Mads Engel Lundt <madslundt@live.dk>
2023-12-21 11:01:08 +0100

    Update updateProperties.ts

--- recursion/updateProperties.ts
+++ recursion/updateProperties.ts
@@ -3,21 +3,28 @@
  * @param {T} source
  * @param {string} key
  * @param {(oldValue: string | number | boolean | null) => string | number | boolean | null} changeFunc
+ * @param {string[]} ignorePathKeys
  * @returns {T}
  */
-const updateProperties = <T extends object | object[]>(source: T, key: string, changeFunc: (oldValue: string | number | boolean | null) => string | number | boolean | null): T => {
-    if (Array.isArray(source)) {
-        return source.map(item => updateProperties(item, key, changeFunc)) as T;
-    } else if (source && typeof source === 'object') {
-        return Object.entries(source).reduce((acc, [sourceKey, value]) => {
-                if (key === sourceKey) {
-                    acc[sourceKey] = changeFunc(value);
-                } else {
-                    acc[sourceKey] = updateProperties(value, key, changeFunc);
-                }
-                return acc;
-            }, {}) as T;
-    }
+const updateProperties = <T extends object | object[]>(
+  source: T, key: string,
+  changeFunc: (oldValue: string | number | boolean | null) => string | number | boolean | null,
+  ignorePathKeys: string[] = []
+): T => {
+  if (Array.isArray(source)) {
+    return source.map(item => updateProperties(item, key, changeFunc)) as T;
+  } else if (source && typeof source === 'object') {
+    return Object.entries(source).reduce((acc, [ sourceKey, value ]) => {
+      if (key === sourceKey) {
+        acc[ sourceKey ] = changeFunc(value);
+      } else if (ignorePathKeys.includes(sourceKey)) {
+        acc[ sourceKey ] = value;
+      } else {
+        acc[ sourceKey ] = updateProperties(value, key, changeFunc);
+      }
+      return acc;
+    }, {}) as T;
+  }
 
-    return source;
+  return source;
 }
